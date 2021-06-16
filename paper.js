b9f4774fc8028983621243c238d88af67472cec1
Advaya patil <79752728+Advaya88@users.noreply.github.com>
2021-06-16 21:17:19 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,39 @@
+class Paper{
+	constructor(x,y,r)
+	{
+	// assign options to the rubber ball
+
+	var options = {
+
+'restitution': 0.3,
+'friction': 5,
+'density': 1
+
+}
+		
+        this.x=x;
+		this.y=y;
+		this.r=r
+		this.image = loadImage("paper.png");
+		this.body=Bodies.circle(this.x, this.y, (this.r-20)/2, options)
+		World.add(world, this.body);
+
+	}
+
+		
+	display()
+	{
+			var paperpos=this.body.position;		
+			push()
+			translate(paperpos.x, paperpos.y);
+			
+			imageMode(CENTER);
+			image(this.image, 0, 0, this.r,this.r);
+			
+			//draw the ellipse here to display the rubber ball
+			//ellipse(0,0,this.r,this.r);
+
+			pop()
+	}
+
+}
